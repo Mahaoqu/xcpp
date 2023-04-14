696627c1ca4c590c0d30edd0c469d7d80dfce5eb
Haoqu Ma <mahaoqu@gmail.com>
2023-04-13 23:56:21 -0400

    add game1

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -4,9 +4,3 @@ TEST(hello, world)
 {
 	EXPECT_EQ(1, 1);
 }
-
-int main(int argc, char **argv)
-{
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_requires("v8", "sfml", "cppzmq", "gtest")
 set_languages("c++17")
 
 target("main")
-    set_kind("binary")
+    set_kind("shared")
     add_files("src/*.cpp")
     add_packages("v8", "sfml", "cppzmq")
     add_defines("V8_COMPRESS_POINTERS")
@@ -11,3 +11,7 @@ target("test")
     set_kind("binary")
     add_files("test/*.cpp")
     add_packages("gtest")
+    if is_plat("windows") then
+        -- fixes "LINK : fatal error LNK1561: entry point must be defined"
+        add_ldflags("/subsystem:console")
+    end
